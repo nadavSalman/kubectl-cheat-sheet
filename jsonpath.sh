0860fddfc601301dd57c1a387bc7a0b2dfd711ce
nsalman <nsalman@attentigroup.com>
2023-03-10 15:31:11 +0200

    add jsonpath query

---
+++ jsonpath.sh
@@ -0,0 +1,352 @@
+Retreive the 3rd month's pay details of the employee.
+
+
+
+Json Input :
+{
+  "employee": {
+    "name": "john",
+    "gender": "male",
+    "age": 24,
+    "address": {
+      "city": "edison",
+      "state": "new jersey",
+      "country": "united states"
+    },
+    "payslips": [
+      {
+        "month": "june",
+        "amount": 1400
+      },
+      {
+        "month": "july",
+        "amount": 2400
+      },
+      {
+        "month": "august",
+        "amount": 3400
+      }
+    ]
+  }
+}
+
+
+Query : $.employee.payslips[?(@.month == "august" )]
+
+
+
+Retreive the 3rd month's pay amount of the employee.
+
+
+
+$.employee.payslips[?(@.month == "august" )].amount 
+[
+  3400
+]
+
+
+
+
+
+Json Input :
+{
+  "prizes": [
+    {
+      "year": "2018",
+      "category": "physics",
+      "overallMotivation": "\"for groundbreaking inventions in the field of laser physics\"",
+      "laureates": [
+        {
+          "id": "960",
+          "firstname": "Arthur",
+          "surname": "Ashkin",
+          "motivation": "\"for the optical tweezers and their application to biological systems\"",
+          "share": "2"
+        },
+        {
+          "id": "961",
+          "firstname": "Gérard",
+          "surname": "Mourou",
+          "motivation": "\"for their method of generating high-intensity, ultra-short optical pulses\"",
+          "share": "4"
+        },
+        {
+          "id": "962",
+          "firstname": "Donna",
+          "surname": "Strickland",
+          "motivation": "\"for their method of generating high-intensity, ultra-short optical pulses\"",
+          "share": "4"
+        }
+      ]
+    },
+    {
+      "year": "2018",
+      "category": "chemistry",
+      "laureates": [
+        {
+          "id": "963",
+          "firstname": "Frances H.",
+          "surname": "Arnold",
+          "motivation": "\"for the directed evolution of enzymes\"",
+          "share": "2"
+        },
+        {
+          "id": "964",
+          "firstname": "George P.",
+          "surname": "Smith",
+          "motivation": "\"for the phage display of peptides and antibodies\"",
+          "share": "4"
+        },
+        {
+          "id": "965",
+          "firstname": "Sir Gregory P.",
+          "surname": "Winter",
+          "motivation": "\"for the phage display of peptides and antibodies\"",
+          "share": "4"
+        }
+      ]
+    },
+    {
+      "year": "2018",
+      "category": "medicine",
+      "laureates": [
+        {
+          "id": "958",
+          "firstname": "James P.",
+          "surname": "Allison",
+          "motivation": "\"for their discovery of cancer therapy by inhibition of negative immune regulation\"",
+          "share": "2"
+        },
+        {
+          "id": "959",
+          "firstname": "Tasuku",
+          "surname": "Honjo",
+          "motivation": "\"for their discovery of cancer therapy by inhibition of negative immune regulation\"",
+          "share": "2"
+        }
+      ]
+    },
+    {
+      "year": "2018",
+      "category": "peace",
+      "laureates": [
+        {
+          "id": "966",
+          "firstname": "Denis",
+          "surname": "Mukwege",
+          "motivation": "\"for their efforts to end the use of sexual violence as a weapon of war and armed conflict\"",
+          "share": "2"
+        },
+        {
+          "id": "967",
+          "firstname": "Nadia",
+          "surname": "Murad",
+          "motivation": "\"for their efforts to end the use of sexual violence as a weapon of war and armed conflict\"",
+          "share": "2"
+        }
+      ]
+    },
+    {
+      "year": "2018",
+      "category": "economics",
+      "laureates": [
+        {
+          "id": "968",
+          "firstname": "William D.",
+          "surname": "Nordhaus",
+          "motivation": "\"for integrating climate change into long-run macroeconomic analysis\"",
+          "share": "2"
+        },
+        {
+          "id": "969",
+          "firstname": "Paul M.",
+          "surname": "Romer",
+          "motivation": "\"for integrating technological innovations into long-run macroeconomic analysis\"",
+          "share": "2"
+        }
+      ]
+    },
+    {
+      "year": "2014",
+      "category": "peace",
+      "laureates": [
+        {
+          "id": "913",
+          "firstname": "Kailash",
+          "surname": "Satyarthi",
+          "motivation": "\"for their struggle against the suppression of children and young people and for the right of all children to education\"",
+          "share": "2"
+        },
+        {
+          "id": "914",
+          "firstname": "Malala",
+          "surname": "Yousafzai",
+          "motivation": "\"for their struggle against the suppression of children and young people and for the right of all children to education\"",
+          "share": "2"
+        }
+      ]
+    },
+    {
+      "year": "2017",
+      "category": "physics",
+      "laureates": [
+        {
+          "id": "941",
+          "firstname": "Rainer",
+          "surname": "Weiss",
+          "motivation": "\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+          "share": "2"
+        },
+        {
+          "id": "942",
+          "firstname": "Barry C.",
+          "surname": "Barish",
+          "motivation": "\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+          "share": "4"
+        },
+        {
+          "id": "943",
+          "firstname": "Kip S.",
+          "surname": "Thorne",
+          "motivation": "\"for decisive contributions to the LIGO detector and the observation of gravitational waves\"",
+          "share": "4"
+        }
+      ]
+    },
+    {
+      "year": "2017",
+      "category": "chemistry",
+      "laureates": [
+        {
+          "id": "944",
+          "firstname": "Jacques",
+          "surname": "Dubochet",
+          "motivation": "\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+          "share": "3"
+        },
+        {
+          "id": "945",
+          "firstname": "Joachim",
+          "surname": "Frank",
+          "motivation": "\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+          "share": "3"
+        },
+        {
+          "id": "946",
+          "firstname": "Richard",
+          "surname": "Henderson",
+          "motivation": "\"for developing cryo-electron microscopy for the high-resolution structure determination of biomolecules in solution\"",
+          "share": "3"
+        }
+      ]
+    },
+    {
+      "year": "2017",
+      "category": "medicine",
+      "laureates": [
+        {
+          "id": "938",
+          "firstname": "Jeffrey C.",
+          "surname": "Hall",
+          "motivation": "\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+          "share": "3"
+        },
+        {
+          "id": "939",
+          "firstname": "Michael",
+          "surname": "Rosbash",
+          "motivation": "\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+          "share": "3"
+        },
+        {
+          "id": "940",
+          "firstname": "Michael W.",
+          "surname": "Young",
+          "motivation": "\"for their discoveries of molecular mechanisms controlling the circadian rhythm\"",
+          "share": "3"
+        }
+      ]
+    }
+  ]
+}
+
+$.prizes.[*].laureates[?( @.firstname == "Malala")]
+
+
+
+
+---
+
+
+{
+  "apiVersion": "v1",
+  "kind": "Pod",
+  "metadata": {
+    "name": "nginx-pod",
+    "namespace": "default"
+  },
+  "spec": {
+    "containers": [
+      {
+        "image": "nginx:alpine",
+        "name": "nginx"
+      },
+      {
+        "image": "redis:alpine",
+        "name": "redis-container"
+      }
+    ],
+    "nodeName": "node01"
+  },
+  "status": {
+    "conditions": [
+      {
+        "lastProbeTime": null,
+        "lastTransitionTime": "2019-06-13T05:34:09Z",
+        "status": "True",
+        "type": "Initialized"
+      },
+      {
+        "lastProbeTime": null,
+        "lastTransitionTime": "2019-06-13T05:34:09Z",
+        "status": "True",
+        "type": "PodScheduled"
+      }
+    ],
+    "containerStatuses": [
+      {
+        "image": "nginx:alpine",
+        "name": "nginx",
+        "ready": false,
+        "restartCount": 4,
+        "state": {
+          "waiting": {
+            "reason": "ContainerCreating"
+          }
+        }
+      },
+      {
+        "image": "redis:alpine",
+        "name": "redis-container",
+        "ready": false,
+        "restartCount": 2,
+        "state": {
+          "waiting": {
+            "reason": "ContainerCreating"
+          }
+        }
+      }
+    ],
+    "hostIP": "172.17.0.75",
+    "phase": "Pending",
+    "qosClass": "BestEffort",
+    "startTime": "2019-06-13T05:34:09Z"
+  }
+}
+
+
+Develop a JSON PATH query to get the restart count of redis-container under the status section.
+
+Scroll down if you can't see the status section
+ 
+$.status.containerStatuses[?( @.name == "redis-container")].restartCount
